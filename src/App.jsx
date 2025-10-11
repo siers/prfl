7b56faacf875758ee85d9781dd16138986c20ce0
Raitis Veinbahs <raitis.veinbahs@dasch.swiss>
2025-10-12 01:41:45 +0200

    handle buttons

--- src/App.jsx
+++ src/App.jsx
@@ -50,20 +50,27 @@ function App() {
     flash()
   }
 
+  const postponeInterval = () => { clearInterval(interval.current); interval.current = setInterval(() => setItem(), 1000) }
   const startInterval = () => { setRunning(true); interval.current = setInterval(() => setItem(), 1000) }
   const stopInterval = () => { setRunning(false); clearInterval(interval.current); console.log('stop'); }
+  const toggleInterval = () => { if (running) stopInterval(); else startInterval() }
 
-  // function button(event, key) {
-  //   if (key == 's' || key == 'Escape') stop(event)
-  //   if (key == 'ArrowRight' || key == 'PageUp' || key == 'ArrowDown' || key == 'Enter' || key == ' ') nextItem()
-  // }
+  function button(event, key) {
+    if (key == 's' || key == 'Escape') toggleInterval()
+    if (key == 'ArrowRight' || key == 'PageUp' || key == 'ArrowDown' || key == 'Enter' || key == ' ') {
+      setItem()
+      postponeInterval()
+    }
+  }
 
   let tone
   let timeoutId
 
   useEffect(() => {
+    const handleKey = (e) => button(event, e.key)
     // addEventListener('hashchange', () => document.location.reload())
-    // addEventListener('keydown', (e) => button(event, e.key))
+    addEventListener('keydown', handleKey)
+    return () => removeEventListener('keydown', handleKey)
   })
 
   useEffect(() => {
@@ -73,8 +80,9 @@ function App() {
 
   return (
     <>
-      {!running && <a className="start" onClick={() => startInterval()}>start</a>}
-      {running && <a className="stop" onClick={() => stopInterval()}>stop</a>}
+      <a className="toggle" onClick={() => toggleInterval()}>
+        {running && "stop" || "start"}
+      </a>
 
       <a className="next" onClick={() => setItem()}>➡️</a>
 
