8fbc89017ea66bc87a04d22bfb8038fa7d8f5180
Raitis Veinbahs <raitis.veinbahs@dasch.swiss>
2025-10-22 20:44:04 +0200

    Violin.jsx: use pick from lib file

--- src/programs/Violin.jsx
+++ src/programs/Violin.jsx
@@ -1,11 +1,8 @@
 import { randomViolinNote } from '../lib/ViolinNote'
-
-function randomItem(array) {
-  return array[Math.floor(Math.random() * array.length)]
-}
+import { pick } from '../lib/Random'
 
 function Violin() {
-  return <div dangerouslySetInnerHTML={{__html: randomItem(randomViolinNote())[1]}} />
+  return <div dangerouslySetInnerHTML={{__html: pick(randomViolinNote())[1]}} />
 }
 
 export default Violin
