92c3704d3edd66ddf981f1d86ddea29d200a946c
Raitis Veinbahs <raitis.veinbahs@dasch.swiss>
2025-12-23 15:18:50 +0100

    index.html: move production state restarter to main.js

---
+++ src/debug.js
@@ -0,0 +1,1 @@
+export const isDev = window.location.host.match(/localhost/)
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './main.css'
 import App from './App.jsx'
+import { isDev } from './debug.js'
+
+import './experiments/exp_20251223_vibe_shuffle.jsx'
+
+setTimeout(() => {
+  if (!isDev && document.getElementById('root').children.length == 0) {
+    localStorage.clear()
+    console.log('cleared local storage')
+  }
+}, 5000)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
