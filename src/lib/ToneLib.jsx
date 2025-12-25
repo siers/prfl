ed224876a76a9d3835b79f133b2fa344b65f98eb
Raitis Veinbahs <raitis.veinbahs@dasch.swiss>
2025-12-25 13:49:56 +0100

    ToneLib: tests

--- src/lib/ToneLib.jsx
+++ src/lib/ToneLib.jsx
@@ -1,9 +1,16 @@
+// # note interface
+// - dat: name (mby enum), alter, octave
+//   derived: semi = c1 + octave * 12 + letter offset
+//   derived: render
+//   derived spelling = (name, alter)
+
 class ToneLib {
   names = 'CDEFGAB'.split('')
-  namesMap = {c: 0, d: 1, e: 2, f: 3, g: 4, a: 5, b: 6}
+  namesMap = { c: 0, d: 1, e: 2, f: 3, g: 4, a: 5, b: 6 }
+  namesSemiMap = { c: 0, d: 2, e: 3, f: 5, g: 7, a: 9, b: 11 }
 
-  alters = {0: '', 1: '#', '-1': 'b', 2: '##', '-2': 'bb'} // todo: use
-  altersMap = {'': 0, '#': 1, 'b': -1, '##': 2, 'bb': -2}
+  alters = { 0: '', 1: '#', '-1': 'b', 2: '##', '-2': 'bb' } // todo: use
+  altersMap = { '': 0, '#': 1, 'b': -1, '##': 2, 'bb': -2 }
 
   arrayShift(arr, count) {
     const len = arr.length
@@ -24,7 +31,7 @@ class ToneLib {
     return Math.floor((note.semi - 4) / 12) + 1
   }
 
-  renderNote({semi, name, alter}, renderOctave = true) {
+  renderNote({ semi, name, alter }, renderOctave = true) {
     const octave = Math.floor((semi - 4) / 12) + 1
     const alt = this.alters[alter] === undefined ? '?' : this.alters[alter]
 
@@ -35,7 +42,7 @@ class ToneLib {
 
   // TODO: this should be a computed property of a Note object
   noteWithRender(note) {
-    return {...note, render: this.renderNote(note)}
+    return { ...note, render: this.renderNote(note), octave: Math.floor((note.semi - 4) / 12) + 1 }
   }
 
   sharp = 1
@@ -52,21 +59,24 @@ class ToneLib {
   octave = 7
   ninth = 8
 
+  c1 = 4
+
   parseNote(note) {
     const match = note.toLowerCase().match(/^(?<note>[abcdefg])(?<accds>[b#]{0,2})?(?<octave>[0-9])?$/)
 
     if (!match) return null
     if (!match.groups.note === undefined) return null
 
-    const semi = parseInt(match.groups.octave || 4, 10) * 12
+    const semiBaseC = parseInt((match.groups.octave || 4) - 1, 10) * 12 + this.c1
+    const semi = semiBaseC + this.namesSemiMap[match.groups.note]
     const name = this.namesMap[match.groups.note]
     const alter = this.altersMap[match.groups.accds || '']
 
-    return this.noteWithRender({semi, name, alter})
+    return this.noteWithRender({ semi, name, alter })
   }
 
-  addAccidental({semi, name, alter}, accidental) {
-    return this.noteWithRender({semi: semi + accidental, name, alter: alter + accidental})
+  addAccidental({ semi, name, alter }, accidental) {
+    return this.noteWithRender({ semi: semi + accidental, name, alter: alter + accidental })
   }
 
   addSharp(note) {
@@ -78,24 +88,16 @@ class ToneLib {
   }
 
   // property key(note, base) = key(note), only octave is changed
-  rebase({semi, name, alter, render}, baseSemi) {
+  rebase({ semi, name, alter, render }, baseSemi) {
     return this.noteWithRender({
-      semi: baseSemi + ((12000 + semi - baseSemi) % 12),
+      semi: baseSemi + ((12000 + semi - this.c1) % 12),
       name,
       alter,
     })
   }
 
   major() {
-    return [
-      {semi: 49 + 3 + 0, name: 0, alter: 0},
-      {semi: 49 + 3 + 2, name: 1, alter: 0},
-      {semi: 49 + 3 + 4, name: 2, alter: 0},
-      {semi: 49 + 3 + 5, name: 3, alter: 0},
-      {semi: 49 + 3 + 7, name: 4, alter: 0},
-      {semi: 49 + 3 + 9, name: 5, alter: 0},
-      {semi: 49 + 3 + 11, name: 6, alter: 0},
-    ].map(n => this.noteWithRender(n))
+    return 'cdefgab'.split('').map(n => this.parseNote(n))
   }
 
   keyAddAccidental(keyOld, n, sign) {
@@ -108,27 +110,25 @@ class ToneLib {
     return key.map(n => this.rebase(n, baseSemi))
   }
 
-  // semitones are unordered, but not names and order is correct
-  modulateFifthBaseless(key, direction) {
-    switch (direction) {
-      case 1: return this.arrayShift(this.keyAddAccidental(key, this.fourth, this.sharp), this.fifth)
-      case -1: return this.arrayShift(this.keyAddAccidental(key, this.seventh, this.flat), this.fourth)
-      case 0: return key
-      default:
-        return this.modulateFifth(this.modulateFifth(key, direction - Math.sign(direction)), Math.sign(direction))
-    }
-  }
-
   modulateFifth(key, direction) {
-    const modulated = this.modulateFifthBaseless(key, direction)
+    const modulated = (() => {
+      switch (direction) {
+        case 1: return this.arrayShift(this.keyAddAccidental(key, this.fourth, this.sharp), this.fifth)
+        case -1: return this.arrayShift(this.keyAddAccidental(key, this.seventh, this.flat), this.fourth)
+        case 0: return key
+        default:
+          return this.modulateFifth(this.modulateFifth(key, direction - Math.sign(direction)), Math.sign(direction))
+      }
+    })()
+
     return this.keyRebase(modulated, modulated[0].semi)
   }
 
   findCommonKey(a, b) {
     var af, bf
     var key = this.keysMajor().find(k => {
-      af = k.find(({semi}) => semi % 12 == a % 12)
-      bf = k.find(({semi}) => semi % 12 == b % 12)
+      af = k.find(({ semi }) => semi % 12 == a % 12)
+      bf = k.find(({ semi }) => semi % 12 == b % 12)
 
       return af && bf
     })
@@ -144,6 +144,10 @@ class ToneLib {
     return [0, 1, -1, 2, -2, 3, -3, 4, -4, 5, -5, 6, -6].map(d => {
       return this.modulateFifth(this.major(), d)
     })
+  }
+
+  majorKey(note) {
+    return this.keysMajor().find(k => this.renderNote(k[0], false) == this.renderNote(note, false))
   }
 
   keysDominant() {
@@ -181,13 +185,9 @@ class ToneLib {
     })
   }
 
-  testParseAndRender() {
-    this.keysMajor().forEach(key => {
-      key.forEach(note => {
-        const renders = [note.render, this.parseNote(note.render).render]
-        if (renders[0] != renders[1]) console.log(`failed parse/render cycle: ${renders}`)
-      })
-    })
+  allNotes() {
+    const c4 = this.parseNote('C4')
+    return this.keysMajor().flat().map(n => this.rebase(n, c4.semi))
   }
 
   // ToneLib.noteKeyAssociations().map(kAndKs => console.log(`${kAndKs[0]}: ${kAndKs[1].join(',')}`))
@@ -227,6 +227,5 @@ class ToneLib {
 
 // https://en.wikipedia.org/wiki/Piano_key_frequencies
 
-window.ToneLib = new ToneLib
+if (typeof window !== 'undefined') window.ToneLib = new ToneLib
 export default ToneLib = new ToneLib
-ToneLib.testParseAndRender()
