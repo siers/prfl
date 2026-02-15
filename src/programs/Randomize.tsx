2420a9b1864fc8b7345cd5961b91fdc899980b44
Raitis Veinbahs <raitis.veinbahs@dasch.swiss>
2026-02-15 18:30:13 +0100

    Randomize: run/commit buttons

--- src/programs/Randomize.tsx
+++ src/programs/Randomize.tsx
@@ -1,5 +1,5 @@
 import { mapParse, mapSerialize } from '../lib/Map.js'
-import { evalContentsMem } from './RandomizeLang.js'
+import { evalContentsMem, Memory } from './RandomizeLang.js'
 import murmur from 'murmurhash3js'
 
 function hm(m: number): string {
@@ -9,36 +9,59 @@ function hm(m: number): string {
 type RState = {
   text?: string,
   distance?: number,
+  execute: boolean,
+
   output?: string,
   outLineCount?: number,
   memory?: string,
+  nextMemory?: string,
 }
+
+type Args = {
+  eval?: boolean,
+  contents?: string,
+  distance?: string,
+  save?: boolean,
+  execute?: boolean,
+}
+
 
 function Randomize(controls: any) {
   const { state, setState } = controls
 
   const distance = state?.distance || 0
 
-  function newAndRecalculate(a: { contents?: string, distance?: string, save?: boolean }) {
-    setState((s: RState) => {
+  function newAndRecalculate(a: Args) {
+    setState((s: RState | undefined) => {
       const contentsOr = a.contents === '' ? a.contents : (a.contents || state?.text || '')
       const distanceOr = parseInt(a.distance || distance)
+      const execute = a.execute === null ? state?.execute : a.execute
 
-      const oldMemory = (state?.memory && mapParse(state.memory)) || new Map()
-      // console.clear()
-      const [lines, memory] = evalContentsMem(contentsOr, oldMemory)
-      const output = lines.join('\n')
-      const outLineCount = output.split('\n').filter(a => a !== '---').length
+      let output = (s?.output || '')
+      let newMemory: Memory | undefined = undefined
 
-      const savedMemory = a.save ? { memory: mapSerialize(memory) } : {}
+      if (a.eval) {
+        const oldMemory = (state?.memory && mapParse(state.memory)) || new Map()
+        // console.clear()
+        const [lines, memory] = evalContentsMem(contentsOr, oldMemory)
+        newMemory = memory
+        output = lines.join('\n')
+      }
+
+      const savedMemory = newMemory !== undefined ? { nextMemory: mapSerialize(newMemory) } : {}
+      const nextMemory = ((s?.nextMemory && a.save) ? { memory: s?.nextMemory, nextMemory: undefined } : {})
 
       return {
         ...s,
         text: contentsOr,
         distance: distanceOr,
-        output,
+
+        output: output === undefined ? s?.output : output,
+        outLineCount: output.split('\n').filter(a => a !== '---').length,
         ...savedMemory,
-        outLineCount,
+        ...nextMemory,
+
+        execute: execute,
       } satisfies RState
     })
   }
@@ -50,9 +73,11 @@ function Randomize(controls: any) {
       </div> */ }
 
       <div className="pl-[10px]">
-        <a className="pr-3" onClick={() => newAndRecalculate({ save: true })}>💾</a>
-        <a className="pr-3" onClick={() => newAndRecalculate({})}>🔄</a>
-        <a className="pr-3" onClick={() => newAndRecalculate({ contents: '' })}>❌{/* right now this breaks history of textarea */}</a>
+        <a className="pr-3" onClick={() => newAndRecalculate({ execute: !state.execute })}>{state?.execute ? '⏸️' : '▶️'}</a>
+        <a className="pr-3" style={state?.nextMemory ? {} : {opacity: '50%'}} onClick={() => newAndRecalculate({ save: true })}>💾</a>
+        <a className="pr-3" onClick={() => newAndRecalculate({ eval: true, })}>🔄</a>
+        <a className="pr-3" onClick={() => newAndRecalculate({ eval: true, contents: '' })}>❌{/* right now this breaks history of textarea */}</a>
+
         <span className="pr-3">
           {state?.outLineCount ? <>{state?.outLineCount} * 4min = {hm(state.outLineCount * 4)}</> : <></>}
         </span>
@@ -63,7 +88,7 @@ function Randomize(controls: any) {
 
       <div className="w-full flex flex-row selection:red text-sm">
         <div className="grow p-[10px]">
-          <textarea className="block w-full p-[5px] border" rows={35} cols={130} onChange={e => newAndRecalculate({ contents: e.target.value })} value={state?.text}></textarea>
+          <textarea className="block w-full p-[5px] border" rows={35} cols={130} onChange={e => newAndRecalculate({ contents: e.target.value, eval: true })} value={state?.text}></textarea>
         </div>
 
         <div className="grow p-[10px]">
@@ -88,8 +113,6 @@ export default Randomize
 // TODO: interpret: 2x = (1/2) + (2/2) (combine with execution mode: the view is just an index of the item + some decoration)
 // TODO: interpret: either text-blocks or text-only line syntax
 // TODO: interpret: use fancy <> for rendering to string, so that it can simply be taken in as a text line, if reinterpreted
-
-// TODO: ui: save button just saves, but doesn't refresh
 
 // TODO: util: schedule doesn't respect
 // TODO: util: if an item doesn't get fulfilled, it still impacted the memory
