bddd9b2774668559002529488b9ab18bc0b4326b
Raitis Veinbahs <raitis.veinbahs@dasch.swiss>
2026-04-13 17:54:01 +0200

    Programs: remove ones superceded by randomize

--- src/Programs.jsx
+++ src/Programs.jsx
@@ -1,8 +1,4 @@
-import Violin from './programs/Violin'
-import Keys from './programs/Keys'
 import Positions from './programs/Positions'
-import Missing from './programs/Missing.tsx'
-import Solfege from './programs/Solfege.tsx'
 import SheetOSMD from './programs/SheetOSMD.tsx'
 import Jcuken from './programs/Jcuken.tsx'
 import Flash from './programs/Flash.tsx'
@@ -11,13 +7,10 @@ import { isDev } from './debug.js'
 
 const programs = {
   positions: Positions,
-  'random keys': Keys,
-  'missing-key': Missing,
-  'solfege-ambiguity': Solfege,
   jcuken: Jcuken,
   flash: Flash,
   randomize: Randomize,
-  ...(isDev ? {sheetosmd: SheetOSMD} : {}),
+  ...(isDev ? { sheetosmd: SheetOSMD } : {}),
 }
 
 export default programs
