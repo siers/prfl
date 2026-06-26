086c519ac8bc397b1aed11ac82ae349b113b0540
Raitis Veinbahs <raitis.veinbahs@dasch.swiss>
2026-06-26 13:17:47 +0200

    ToneLib: section cleanup

--- src/lib/ToneLib.ts
+++ src/lib/ToneLib.ts
@@ -32,15 +32,6 @@ export interface Note {
 export type Key = Note[] // semantic distinction only, unfortunately
 type Notes = Note[]
 
-export function semi(n: Note): number {
-  return 4 + (n.octave - 1) * 12 + namesSemiMap[n.name] + n.alter
-}
-
-export function render(n: Note, octave: Boolean = true): string {
-  const alt = alters[n.alter] ?? '?'
-  return n.name.toUpperCase() + alt + (octave ? n.octave : '')
-}
-
 const sharp = 1
 const flat = -1
 
@@ -58,6 +49,8 @@ const ninth = 8
 const c4: Note = parseNote('c')!
 const c1: Note = parseNote('c1')!
 
+// === § Notes
+
 export function parseNote(note: string): Note | null {
   const match = note.toLowerCase().match(/^(?<note>[abcdefg])(?<accds>[b#]{0,2})?(?<octave>[0-9])?$/)
 
@@ -71,6 +64,10 @@ export function parseNote(note: string): Note | null {
   return { name, alter, octave }
 }
 
+export function equalNote(a: Note, b: Note): boolean {
+  return a.name == b.name && a.alter == b.alter
+}
+
 function signedHash(value: number, positivePrime: number, negativePrime: number): number {
   const prime = value >= 0 ? positivePrime : negativePrime
   return prime ** Math.abs(value)
@@ -80,14 +77,28 @@ function hashNote(n: Note): number {
   return 2 ** names.indexOf(n.name) * signedHash(n.alter, 3, 5) * 7 ** n.octave
 }
 
-// I hate javascript
-function uniqueNotes(n: Notes): Notes {
-  const uniq: Record<number, Note> = {}
-  n.forEach(n => uniq[hashNote(n)] = n)
-  return Object.values(uniq)
+// respell a note by its letter into a specific key
+// if you give an ill-defined key, it will blow up
+export function rename(a: Note, k: Key) {
+  return { ...a, alter: k.find(n => n.name == a.name)!.alter }
 }
 
-const allNotesGenerated: Note[] = allNotes()
+export function semi(n: Note): number {
+  return 4 + (n.octave - 1) * 12 + namesSemiMap[n.name] + n.alter
+}
+
+export function render(n: Note, octave: Boolean = true): string {
+  const alt = alters[n.alter] ?? '?'
+  return n.name.toUpperCase() + alt + (octave ? n.octave : '')
+}
+
+export function addAccidental(note: Note, accidental: number): Note {
+  return { ...note, alter: note.alter + accidental }
+}
+
+function semiOctaves(semiIn: number): number {
+  return Math.floor((semiIn - semi(c1)) / 12) + 1
+}
 
 export function enharmonics(semiTarget: number): Note[] {
   const spectrum = uniqueNotes([...allNotesGenerated].map(normalize))
@@ -96,18 +107,20 @@ export function enharmonics(semiTarget: number): Note[] {
   return found.map(n => ({ ...n, octave: semiOctaves(semiTarget) }))
 }
 
-function semiOctaves(semiIn: number): number {
-  return Math.floor((semiIn - semi(c1)) / 12) + 1
+// old, wrong comment: property key(note, base) = key(note), only octave is changed
+export function rebase(note: Note, base: Note): Note {
+  const distanceA = names.indexOf(note.name) // * base.octave * 8
+  const distanceB = names.indexOf(base.name) // * base.octave * 8
+  const before = distanceA < distanceB
+  return { name: note.name, alter: note.alter, octave: base.octave + (before ? 1 : 0) }
 }
 
-function addAccidental(note: Note, accidental: number): Note {
-  return { ...note, alter: note.alter + accidental }
+export function normalize(n: Note): Note {
+  return rebase(n, c4)
 }
 
-export function addInterval(note: Note, interval: number): Note {
-  if (interval == 0) return note
-  if (Math.abs(interval) > 200) return note
-  return times(Math.abs(interval)).reduce((n) => interval > 0 ? stepUp(n) : stepDown(n), note)
+export function rebaseSemi(note: Note, semiBase: number): Note {
+  return rebase(note, enharmonics(semiBase)[0])
 }
 
 function stepUp(n: Note): Note {
@@ -128,38 +141,60 @@ function stepDown(n: Note): Note {
   }
 }
 
-// semi here should be semantically wrong
-function stepTo(n: Note, to: Note): Note {
+// semi here should be semantically wrong, because semi is an embedding of a note, a note itself
+export function stepTo(n: Note, to: Note): Note {
   const next = semi(to) >= semi(n) ? stepUp(n) : stepDown(n)
   if (next.name === to.name) return { ...next, alter: to.alter }
   return next
 }
 
+export function addInterval(note: Note, interval: number): Note {
+  if (interval == 0) return note
+  if (Math.abs(interval) > 200) return note
+  return times(Math.abs(interval)).reduce((n) => interval > 0 ? stepUp(n) : stepDown(n), note)
+}
+
+// === § Note collections
+
+// I hate javascript
+function uniqueNotes(n: Notes): Notes {
+  const uniq: Record<number, Note> = {}
+  n.forEach(n => uniq[hashNote(n)] = n)
+  return Object.values(uniq)
+}
+
+const allNotesGenerated: Note[] = allNotes()
+
+export function allNotes(): Notes {
+  return keysMajor().flat()
+}
+
+export function normalizedNotesRendered(k: Notes): Set<string> {
+  return Set(k.map(n => render(normalize(n), false)))
+}
+
+export function allNotesRendered(): Set<string> {
+  return normalizedNotesRendered(keysMajor().flat())
+}
+
+// === § Intervals
+
 export function nameInterval(int: number): string {
   return intervalMap[Math.abs(int) % 7]
 }
 
-// old, wrong comment: property key(note, base) = key(note), only octave is changed
-export function rebase(note: Note, base: Note): Note {
-  const distanceA = names.indexOf(note.name) // * base.octave * 8
-  const distanceB = names.indexOf(base.name) // * base.octave * 8
-  const before = distanceA < distanceB
-  return { name: note.name, alter: note.alter, octave: base.octave + (before ? 1 : 0) }
+export function pointwiseInterval(a: Note, b: Note, ...cs: Note[]): Note[] {
+  if (a.name === b.name && a.octave === b.octave) return []
+  const next = stepTo(a, b)
+  if (next.name === b.name && next.octave === b.octave && cs.length == 0) return [a, b]
+  if (next.name === b.name && next.octave === b.octave && cs.length != 0) {
+    const [c, ...css] = cs
+    return [a, ...pointwiseInterval(b, c, ...css)]
+  }
+  return [a, ...pointwiseInterval(next, b, ...cs)]
 }
 
-export function normalize(n: Note): Note {
-  return rebase(n, c4)
-}
-
-export function rebaseSemi(note: Note, semiBase: number): Note {
-  return rebase(note, enharmonics(semiBase)[0])
-}
-
-export function equalNote(a: Note, b: Note): boolean {
-  return a.name == b.name && a.alter == b.alter
-}
-
-// === Keys
+// === § Keys
 
 function keyAddAccidental(key_: Key, n: number, sign: number): Key {
   const key = structuredClone(key_)
@@ -199,76 +234,8 @@ export function findMajor(tonic: Note): Key | undefined {
   return keysMajor().find(k => equalNote(rebase(k[0], c4), rebase(tonic, c4)))
 }
 
-// === Utilities & Computations
-
-export function findCommonKey(a: number, b: number): [Key, [Note, Note]] | undefined {
-  var af, bf
-  var key = keysMajor().find(k => {
-    af = k.find((note: Note) => semi(note) % 12 == a % 12)
-    bf = k.find((note: Note) => semi(note) % 12 == b % 12)
-
-    return af && bf
-  })
-
-  if (key) {
-    return [key, [af!, bf!]]
-  } else {
-    console.log(`finding common key failed for ${a}/${b}`)
-  }
-}
-
 export function majorKey(note: Note): Key | undefined {
   return keysMajor().find(k => render(k[0], false) == render(note, false))
-}
-
-function keysDominant() {
-  return keysMajor().map(k => keyAddAccidental(k, seventh, -1))
-}
-
-function keysDominantFlatNine() {
-  return keysDominant().map(k => keyAddAccidental(k, second, -1))
-}
-
-export function nameLeadingDim7(): string[] {
-  return keysDominantFlatNine().map(k => [third, fifth, seventh, ninth % octave].map(n => render(k[n])).join(' '))
-}
-
-// export function noteKeyAssociations(sort = true) {
-//   const keys = {}
-//   keysMajor().forEach(key =>
-//     key.forEach(note => {
-//       const r = render(note, false)
-//       keys[r] = (keys[r] || []).concat([render(key[0], false)])
-//     })
-//   )
-//   const sorted = Object.entries(keys).sort((a, b) => {
-//     const cmp = tonic => {
-//       const s = tonic.indexOf('#') != -1 && 100
-//       const f = tonic.indexOf('b') != -1 && -100
-//       const weights = "CDEFGAB".indexOf(tonic[0])
-//       return s + f + weights
-//     }
-//     cmp(b) - cmp(a)
-//   })
-//   return sort ? sorted : Object.entries(keys).sort((a, b) => {
-//     return "CDEFGAB".indexOf(a[0][0]) - "CDEFGAB".indexOf(b[0][0])
-//   })
-// }
-
-export function allNotes(): Notes {
-  return keysMajor().flat()
-}
-
-export function allNotesRendered(): Set<string> {
-  return normalizedNotesRendered(keysMajor().flat())
-}
-
-export function normalizedNotesRendered(k: Notes): Set<string> {
-  return Set(k.map(n => render(normalize(n), false)))
-}
-
-export function notesMissing(k: Key, l: Key): Set<string> {
-  return allNotesRendered().subtract(normalizedNotesRendered(k).union(normalizedNotesRendered(l)))
 }
 
 export function keyCenters(mode: number = 0): Note[] {
@@ -286,19 +253,36 @@ export function keysBySemi(n: number): Note[] {
   return enharmonics(n).flatMap(e => centers.filter(c => equalNote(c, e)))
 }
 
-export function pointwiseInterval(a: Note, b: Note, ...cs: Note[]): Note[] {
-  if (a.name === b.name && a.octave === b.octave) return []
-  const next = stepTo(a, b)
-  if (next.name === b.name && next.octave === b.octave && cs.length == 0) return [a, b]
-  if (next.name === b.name && next.octave === b.octave && cs.length != 0) {
-    const [c, ...css] = cs
-    return [a, ...pointwiseInterval(b, c, ...css)]
-  }
-  return [a, ...pointwiseInterval(next, b, ...cs)]
+function keysDominant() {
+  return keysMajor().map(k => keyAddAccidental(k, seventh, -1))
 }
 
-// respell a note by its letter into a specific key
-// if you give an ill-defined key, it will blow up
-export function rename(a: Note, k: Key) {
-  return { ...a, alter: k.find(n => n.name == a.name)!.alter }
+function keysDominantFlatNine() {
+  return keysDominant().map(k => keyAddAccidental(k, second, -1))
+}
+
+export function findCommonKey(a: number, b: number): [Key, [Note, Note]] | undefined {
+  var af, bf
+  var key = keysMajor().find(k => {
+    af = k.find((note: Note) => semi(note) % 12 == a % 12)
+    bf = k.find((note: Note) => semi(note) % 12 == b % 12)
+
+    return af && bf
+  })
+
+  if (key) {
+    return [key, [af!, bf!]]
+  } else {
+    console.log(`finding common key failed for ${a}/${b}`)
+  }
+}
+
+export function nameLeadingDim7(): string[] {
+  return keysDominantFlatNine().map(k => [third, fifth, seventh, ninth % octave].map(n => render(k[n])).join(' '))
+}
+
+// === § Quiz
+
+export function notesMissing(k: Key, l: Key): Set<string> {
+  return allNotesRendered().subtract(normalizedNotesRendered(k).union(normalizedNotesRendered(l)))
 }
