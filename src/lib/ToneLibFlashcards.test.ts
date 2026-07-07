8d34ccd667f41cbdc328498049dbea85926bc0e0
Raitis Veinbahs <raitis.veinbahs@dasch.swiss>
2026-07-07 18:32:02 +0200

    Flashcards

--- src/lib/ToneLibFlashcards.test.ts
+++ src/lib/ToneLibFlashcards.test.ts
@@ -1,7 +1,19 @@
 import { test } from 'vitest'
-import { flashcardsCsv } from './ToneLibFlashcards'
+// import { flashcardsDegrees } from './ToneLibFlashcards'
+// import { flashcardsPosition } from './ToneLibFlashcards'
+import { flashcardsNeighbors, flashcardsToCsv } from './ToneLibFlashcards'
 
-test('flashcards', () => {
-  // console.log
-  (flashcardsCsv().join('\n'))
-})
+// uncomment, when export to anki needed
+// test('flashcards degrees', () => {
+//   console.log(flashcardsToCsv(flashcardsDegrees()).join('\n'))
+// })
+
+// uncomment, when export to anki needed
+// test('flashcards position', () => {
+//   console.log(flashcardsToCsv(flashcardsPosition()).join('\n'))
+// })
+
+// uncomment, when export to anki needed
+// test('flashcards neighbors', () => {
+//   console.log(flashcardsToCsv(flashcardsNeighbors()).join('\n'))
+// })
--- src/lib/ToneLibFlashcards.ts
+++ src/lib/ToneLibFlashcards.ts
@@ -1,5 +1,8 @@
 import _ from 'lodash'
 import { render, addInterval, majorKey, parseNote, nameInterval, names, Note } from './ToneLib'
+import { strings } from './ToneLibViolin'
+import { transpose } from './Array'
+import * as Comb from 'ts-combinatorics'
 
 const c = majorKey(parseNote('c')!)!
 const degrees = [1, 2, 3, 4, 5, 6, 7]
@@ -7,7 +10,7 @@ const intervals = [1, 2, 3, 4, 5, 6, -1, -2, -3, -4, -5, -6]
 
 type QA = [string, string]
 
-export function flashcards(): QA[] {
+export function flashcardsDegrees(): QA[] {
   return c.flatMap(note => {
     return intervals.flatMap(int => {
       const added = addInterval(note, int)
@@ -47,6 +50,29 @@ export function flashcards(): QA[] {
   })
 }
 
-export function flashcardsCsv(): string[] {
-  return _.uniq(flashcards().map(([q, a]) => `${q},${a}`))
+export function flashcardsToCsv(cards: QA[]): string[] {
+  return _.uniq(cards.map(([q, a]) => `${q},${a}`))
+}
+
+export function flashcardsNeighbors(): QA[] {
+  const intervals = [1, -1, 4, -4]
+
+  return names.flatMap(name => {
+    const note = parseNote(name)!
+    const neighborsFlat = intervals.flatMap(int => render(addInterval(note, int), false)).join('')
+
+    return [...new Comb.Permutation(neighborsFlat)].map(neighbors =>
+      [neighbors.join(''), render(note, false)] satisfies QA
+    )
+  })
+}
+
+export function flashcardsPosition(): QA[] {
+  return transpose(strings.map(s => s.positions)).slice(0, 8).flatMap((positionFlat, index) => {
+    return [...new Comb.Permutation(positionFlat)].map(position => {
+      const notes = position.map(n => render(n, false)).join('')
+
+      return [notes, `${index}`] satisfies QA
+    })
+  })
 }
