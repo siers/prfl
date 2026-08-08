cc153d7f9259925b5b79f012dbf334feda982865
Raitis Veinbahs <raitis.veinbahs@dasch.swiss>
2026-08-08 20:20:29 +0200

    Flashcards: interval + shift = finger table

--- src/lib/ToneLibFlashcards.ts
+++ src/lib/ToneLibFlashcards.ts
@@ -76,3 +76,53 @@ export function flashcardsPosition(): QA[] {
     })
   })
 }
+
+//
+
+export function pad(str: string, size: number, padF: (_: string) => string): string {
+  var s = str
+  while (s.length < size) s = padF(s)
+  return s
+}
+
+type ShiftSig = (dir: number, start: number, shift: number, interval: number, landing: number) => boolean
+
+// to generate a reference sheet displayed in tests only
+export function fingerShiftInterval(f: ShiftSig = (_a, _b, _c, _d, _e) => true): string[] {
+  const intervalMap: Record<number, string> = {
+    0: 'unison',
+    1: 'second',
+    2: 'third',
+    3: 'fourth',
+    4: 'fifth',
+    5: 'sixth',
+    6: 'seventh',
+  }
+
+  const qa = [1, -1].flatMap(direction => {
+    return [1, 2, 3, 4].flatMap(start => {
+      return [1, 2, 3, 4].flatMap(shift => {
+        return [1, 2, 3, 4].flatMap(interval => {
+          const landingOut = start + (interval - shift) * direction
+          const landingReal = landingOut < 1 || landingOut > 4 ? 0 : landingOut
+          const landingName = landingReal == 0 ? 'x' : landingReal
+          const dir = direction > 0 ? '+' : '-'
+          if (!f(direction, start, shift, interval, landingReal)) return []
+          else return [`${start} ${dir} ${shift}s ${dir} ${pad(intervalMap[interval], 7, x => x + ' ')} = ${landingName}`]
+        })
+      })
+    })
+  })
+
+  const all = qa.map((qa, idx) => `${pad(`${idx}:`, 4, s => s + ' ')} ${qa}`)
+
+  return all
+}
+
+export function fingerShiftIntervalShort(): string[] {
+  return fingerShiftInterval((_dir: number, _start: number, shift: number, interval: number, landing: number) => {
+    if (landing == 0) return false
+    if (shift == interval) return false
+    return true
+  })
+}
